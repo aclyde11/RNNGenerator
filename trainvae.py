b76442d39bb827aa6ccf43a81750282437c23b57
Austin Clyde <aclyde@uchicago.edu>
2019-10-08 13:05:45 -0500

    converted to gpu code

--- trainvae.py
+++ trainvae.py
@@ -187,6 +187,9 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Device: ", device)
     path = args.logdir
+
+    import torch
+    torch.autograd.set_detect_anomaly(True)
     try:
         os.mkdir(path)
     except OSError:
