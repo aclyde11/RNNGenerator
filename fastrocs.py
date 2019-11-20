881383adf87369dc2aca8988c1d5d6bb2478b12c
Austin Clyde <aclyde@uchicago.edu>
2019-11-20 07:02:53 -0600

    converted to gpu code

--- fastrocs.py
+++ fastrocs.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
-# (C) 2017 OpenEye Scientific Software Inc. All rights reserved.
-#
-# TERMS FOR USE OF SAMPLE CODE The software below ("Sample Code") is
-# provided to current licensees or subscribers of OpenEye products or
-# SaaS offerings (each a "Customer").
-# Customer is hereby permitted to use, copy, and modify the Sample Code,
-# subject to these terms. OpenEye claims no rights to Customer's
-# modifications. Modification of Sample Code is at Customer's sole and
-# exclusive risk. Sample Code may require Customer to have a then
-# current license or subscription to the applicable OpenEye offering.
-# THE SAMPLE CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED.  OPENEYE DISCLAIMS ALL WARRANTIES, INCLUDING, BUT
-# NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. In no event shall OpenEye be
-# liable for any damages or liability in connection with the Sample Code
-# or its use.
-
 from __future__ import print_function
 import os
 import sys
@@ -85,10 +67,7 @@ def getargs():
     parser.add_argument('-o', type=str, required=True)
     return parser.parse_args()
 
-def main(argv=[__name__]):
-    if len(argv) < 3:
-        oechem.OEThrow.Usage("%s <database> [<queries> ... ]" % argv[0])
-
+def main():
     if not oefastrocs.OEFastROCSIsGPUReady():
         oechem.OEThrow.Info("No supported GPU available!")
         return 0
@@ -143,4 +122,4 @@ def main(argv=[__name__]):
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    main()
