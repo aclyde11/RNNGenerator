83051facadef558c986e197eefd21d580c70da1c
Austin Clyde <aclyde@uchicago.edu>
2019-11-20 14:43:34 -0600

    converted to gpu code

--- simsearch.py
+++ simsearch.py
@@ -7,7 +7,17 @@ from rdkit import DataStructs
 from rdkit.Chem import AllChem
 from tqdm import tqdm
 
+def getmaxsmi(smi):
+    smi = smi.strip()
+    m = Chem.MolFromSmiles(smi)
+    if m is None:
+        return np.nan
 
+    fp2 = AllChem.GetMorganFingerprint(m, 2)
+    max_sim = 0
+    for fp1 in fps:
+        max_sim = max(max_sim, DataStructs.TanimotoSimilarity(fp1, fp2))
+    return max_sim
 
 def compute_fp_dict(source="kinasesmiles/john_smiles_kinasei.smi"):
     with open(source, 'r') as f:
@@ -26,6 +36,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', required=True, type=str)
     parser.add_argument('-o', required=True, type=str)
+    parser.add_argument('-n', required=False, default=1, type=int)
     return parser.parse_args()
 
 args = get_args()
@@ -35,19 +46,12 @@ print("loaded fps")
 
 df = pd.read_csv(args.i)
 print("loaded", df.shape[0], 'smiles')
-res = []
-for smi in tqdm(df.loc[:, 'smiles'].tolist()):
-    smi = smi.strip()
-    m = Chem.MolFromSmiles(smi)
-    if m is None:
-        res.append(np.nan)
-        continue
 
-    fp2 = AllChem.GetMorganFingerprint(m, 2)
-    max_sim = 0
-    for fp1 in fps:
-        max_sim = max(max_sim, DataStructs.TanimotoSimilarity(fp1, fp2))
-    res.append(max_sim)
+import multiprocessing
+with multiprocessing.Pool(args.n) as p:
+    it = p.imap(getmaxsmi, df.loc[:, 'smiles'].tolist())
+    res = list(tqdm(it))
+
 
 df['sim'] = res
 
