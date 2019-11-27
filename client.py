529a13965c565d74bb5706eeda6f6e9571013b5b
Austin Clyde <aclyde@uchicago.edu>
2019-11-27 07:51:05 -0600

    converted to gpu code

--- client.py
+++ client.py
@@ -21,11 +21,18 @@ import os
 import sys
 import argparse
 
-from openeye import oechem
-from openeye import oefastrocs
+try:
+    from xmlrpclib import ServerProxy, Binary, Fault
+except ImportError:  # python 3
+    from xmlrpc.client import ServerProxy, Binary, Fault
 
-oepy = os.path.join(os.path.dirname(__file__), "..", "python")
-sys.path.insert(0, os.path.realpath(oepy))
+
+def GetFormatExtension(fname):
+    base, ext = os.path.splitext(fname.lower())
+    if ext == ".gz":
+        base, ext = os.path.splitext(base)
+        ext += ".gz"
+    return ext
 
 
 def main(argv=[__name__]):
@@ -33,112 +40,111 @@ def main(argv=[__name__]):
     parser = argparse.ArgumentParser()
 
     # positional arguments retaining backward compatibility
-    parser.add_argument('database',
-                        help='File containing the database molecules to be search \
-                              (format not restricted to *.oeb).')
-    parser.add_argument('query', default=[], nargs='+',
-                        help='File containing the query molecule(s) to be search \
-                              (format not restricted to *.oeb).')
-    parser.add_argument('--nHits', dest='nHits', type=int, default=100,
-                        help='Number of hits to return (default = number of database mols).')
-    parser.add_argument('--cutoff',  dest='cutoff', type=float, default=argparse.SUPPRESS,
-                        help='Specify a cutoff criteria for scores.')
-    parser.add_argument('--tversky', dest='tversky', action='store_true', default=argparse.SUPPRESS,
-                        help='Switch to Tversky similarity scoring (default = Tanimoto).')
+    parser.add_argument('server:port', help='Server name and port number of database to search '
+                                            'i.e. localhost:8080.')
+    parser.add_argument('query', help='File containing the query molecule to search '
+                                      '(format not restricted to *.oeb).')
+    parser.add_argument('results',
+                        help='Output file to store results (format not restricted to *.oeb).')
+    parser.add_argument('nHits',  nargs='?', type=int, default=100,
+                        help='Number of hits to return (default=100).')
+    parser.add_argument('--tversky', action='store_true', default=argparse.SUPPRESS,
+                        help='Switch to Tversky similarity scoring (default=Tanimoto).')
+    parser.add_argument('--shapeOnly', action='store_true', default=argparse.SUPPRESS,
+                        help='Switch to shape-only scores (default=Combo).')
+    parser.add_argument('--alternativeStarts', default=argparse.SUPPRESS, nargs=1, dest='altStarts',
+                        choices=('random', 'subrocs',
+                                 'inertialAtHeavyAtoms', 'inertialAtColorAtoms'),
+                        help='Optimize using alternative starts (default=inertial). '
+                             'To perform N random starts do '
+                             '"--alternativeStarts random N" (default N=10)')
 
-    args = parser.parse_args()
+    known, remaining = (parser.parse_known_args())
+    dargs = vars(known)
 
-    dbname = args.database
+    qfname = dargs.pop('query')
+    numHits = dargs.pop('nHits')
 
-    if not oefastrocs.OEFastROCSIsGPUReady():
-        oechem.OEThrow.Info("No supported GPU available!")
-        return 0
+    startType = dargs.get('altStarts', None)
 
-    # set options
-    opts = oefastrocs.OEShapeDatabaseOptions()
-    opts.SetLimit(args.nHits)
-    print("Number of hits set to %u" % opts.GetLimit())
-    if hasattr(args, 'cutoff') is not False:
-        opts.SetCutoff(args.cutoff)
-        print("Cutoff set to %f" % args.cutoff)
-    if hasattr(args, 'tversky') is not False:
-        opts.SetSimFunc(args.tversky)
-        print("Tversky similarity scoring set.")
+    if startType:
+        dargs['altStarts'] = str(startType[0])
+        if len(remaining) == 1 and dargs['altStarts'] == 'random':
+            try:
+                numRands = int(remaining[0])
+                dargs['randStarts'] = numRands
+            except ValueError:
+                print("Invalid argument given. See --help menu for argument list")
+                sys.exit()
+        if len(remaining) > 1:
+            print("Too many arguments given. See --help menu for argument list")
+            sys.exit()
+    else:
+        if remaining:
+            print("Too many arguments given. See --help menu for argument list")
+            sys.exit()
 
-    # read in database
-    ifs = oechem.oemolistream()
-    if not ifs.open(dbname):
-        oechem.OEThrow.Fatal("Unable to open '%s'" % dbname)
+    try:
+        fh = open(qfname, 'rb')
+    except IOError:
+        sys.stderr.write("Unable to open '%s' for reading" % qfname)
+        return 1
 
-    print("\nOpening database file %s ..." % dbname)
-    timer = oechem.OEWallTimer()
-    dbase = oefastrocs.OEShapeDatabase()
-    moldb = oechem.OEMolDatabase()
-    if not moldb.Open(ifs):
-        oechem.OEThrow.Fatal("Unable to open '%s'" % dbname)
+    iformat = GetFormatExtension(qfname)
 
-    dots = oechem.OEThreadedDots(10000, 200, "conformers")
-    if not dbase.Open(moldb, dots):
-        oechem.OEThrow.Fatal("Unable to initialize OEShapeDatabase on '%s'" % dbname)
+    ofname = dargs.pop('results')
+    oformat = GetFormatExtension(ofname)
 
-    dots.Total()
-    print("%f seconds to load database\n" % timer.Elapsed())
+    s = ServerProxy("http://" + dargs.pop('server:port'))
+    data = Binary(fh.read())
 
-    for qfname in args.query:
+    try:
+        idx = s.SubmitQuery(data, numHits, iformat, oformat, dargs)
+    except Fault as e:
+        if "TypeError" in e.faultString:
+            # we're trying to run against an older server, may be able
+            # to still work if the formats ameniable.
+            if ((iformat == ".oeb" or iformat == ".sq") and oformat == ".oeb"):
+                idx = s.SubmitQuery(data, numHits)
+            else:
+                sys.stderr.write("%s is too new of a version to work with the server %s\n"
+                                 % (argv[0], argv[1]))
+                sys.stderr.write("Please upgrade your server to FastROCS version 1.4.0"
+                                 " or later to be able to use this client\n")
+                sys.stderr.write("This client will work with this version of the server "
+                                 "if the input file is either"
+                                 "'.oeb' or '.sq' and the output file is '.oeb'\n")
+                return 1
+        else:
+            sys.stderr.write(str(e))
+            return 1
 
-        # read in query
-        qfs = oechem.oemolistream()
-        if not qfs.open(qfname):
-            oechem.OEThrow.Fatal("Unable to open '%s'" % qfname)
+    first = False
+    while True:
+        blocking = True
+        try:
+            current, total = s.QueryStatus(idx, blocking)
+        except Fault as e:
+            print(str(e), file=sys.stderr)
+            return 1
 
-        mcmol = oechem.OEMol()
-        if not oechem.OEReadMolecule(qfs, mcmol):
-            oechem.OEThrow.Fatal("Unable to read query from '%s'" % qfname)
-        qfs.rewind()
+        if total == 0:
+            continue
 
-        ext = oechem.OEGetFileExtension(qfname)
+        if first:
+            print("%s/%s" % ("current", "total"))
+            first = False
+        print("%i/%i" % (current, total))
 
-        qmolidx = 0
-        while oechem.OEReadMolecule(qfs, mcmol):
+        if total <= current:
+            break
 
-            # write out to file name based on molecule title
-            ofs = oechem.oemolostream()
-            moltitle = mcmol.GetTitle()
-            if len(moltitle) == 0:
-                moltitle = str(qmolidx)
-            ofname = moltitle + "_results." + ext
-            if not ofs.open(ofname):
-                oechem.OEThrow.Fatal("Unable to open '%s'" % argv[4])
+    results = s.QueryResults(idx)
 
-            print("Searching for %s of %s (%s conformers)" % (moltitle, qfname, mcmol.NumConfs()))
+    # assuming the results come back as a string in the requested format
+    with open(ofname, 'wb') as output:
+        output.write(results.data)
 
-            qconfidx = 0
-            for conf in mcmol.GetConfs():
-
-                for score in dbase.GetSortedScores(conf, opts):
-
-                    dbmol = oechem.OEMol()
-                    dbmolidx = score.GetMolIdx()
-                    if not moldb.GetMolecule(dbmol, dbmolidx):
-                        print("Unable to retrieve molecule '%u' from the database" % dbmolidx)
-                        continue
-
-                    mol = oechem.OEGraphMol(dbmol.GetConf(oechem.OEHasConfIdx(score.GetConfIdx())))
-
-                    oechem.OESetSDData(mol, "QueryConfidx", "%s" % qconfidx)
-                    oechem.OESetSDData(mol, "ShapeTanimoto", "%.4f" % score.GetShapeTanimoto())
-                    oechem.OESetSDData(mol, "ColorTanimoto", "%.4f" % score.GetColorTanimoto())
-                    oechem.OESetSDData(mol, "TanimotoCombo", "%.4f" % score.GetTanimotoCombo())
-                    score.Transform(mol)
-
-                    oechem.OEWriteMolecule(ofs, mol)
-
-                qconfidx += 1
-
-            print("%s conformers processed" % qconfidx)
-            print("Wrote results to %s\n" % ofname)
-
-        qmolidx += 1
     return 0
 
 
