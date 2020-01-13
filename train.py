538d9b045bcceeb517c469b3a3f096b179e2c440
Austin Clyde <aclyde@uchicago.edu>
2020-01-13 12:11:40 -0600

    converted to gpu code

--- train.py
+++ train.py
@@ -46,8 +46,8 @@ def sample(model, i2c, c2i, device, z_dim=2, temp=1, batch_size=10, max_len=320)
     model.eval()
     with torch.no_grad():
 
-        c_0 = torch.zeros((8, batch_size, 256)).to(device)
-        h_0 = torch.zeros((8, batch_size, 256)).to(device)
+        c_0 = torch.zeros((6, batch_size, 256)).to(device)
+        h_0 = torch.zeros((6, batch_size, 256)).to(device)
         x = torch.tensor(c2i(START_CHAR)).unsqueeze(0).unsqueeze(0).repeat((max_len, batch_size)).to(device)
 
         eos_mask = torch.zeros(batch_size, dtype=torch.bool).to(device)
