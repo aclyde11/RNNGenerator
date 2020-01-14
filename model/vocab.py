4381cad5341301eae95da0ba96a0d53b6eaab310
Austin Clyde <aclyde@uchicago.edu>
2020-01-14 12:44:45 -0600

    converted to gpu code

--- model/vocab.py
+++ model/vocab.py
@@ -56,33 +56,34 @@ def main(args):
             randomSmiles('CNOPc1ccccc1', 10)
         except:
             print("Must set --permute_smiles to 0, cannot import RdKit. Smiles validity not being checked either.")
-    #
-    # #first step generate vocab.
-    # count = 0
-    # vocab = set()
-    # vocab.update([START_CHAR, END_CHAR])
-    # print(vocab)
-    # with open(args.i, 'r') as f:
-    #     for line in f:
-    #         smi = line.strip()
-    #         count += 1
-    #         if len(smi) > args.maxlen - 2:
-    #             continue
-    #         vocab.update(smi)
-    #
-    # vocab = list(vocab)
-    # with open(args.o + '/vocab.txt', 'w') as f:
-    #     for v in vocab:
-    #         f.write(v + '\n')
-    #
-    #
-    # print("Read ", count,"smiles.")
-    # print("Vocab length: ", len(vocab), "Max len: ", args.maxlen)
 
-    #seconnd step is to make data:
-    # count_=count
-    count = 0
+    #first step generate vocab.
+    if args.start:
+        count = 0
+        vocab = set()
+        vocab.update([START_CHAR, END_CHAR])
+        print(vocab)
+        with open(args.i, 'r') as f:
+            for line in f:
+                smi = line.strip()
+                count += 1
+                if len(smi) > args.maxlen - 2:
+                    continue
+                vocab.update(smi)
+
+        vocab = list(vocab)
+        with open(args.o + '/vocab.txt', 'w') as f:
+            for v in vocab:
+                f.write(v + '\n')
+
+
+        print("Read ", count,"smiles.")
+        print("Vocab length: ", len(vocab), "Max len: ", args.maxlen)
     _, c2i, _ = get_vocab_from_file(args.o + '/vocab.txt')
+
+    # seconnd step is to make data:
+    count_=count
+    count = 0
     with open(args.i, 'r') as f:
         with open(args.o + '/out.txt', 'w') as o:
             for line in tqdm(f):
@@ -114,6 +115,7 @@ if __name__ == '__main__':
     parser.add_argument('-o', type=str, required=True, help='output directory where preprocressed data and vocab will go')
     parser.add_argument('--maxlen', type=int, required=True, help='max length for smile strings to be')
     parser.add_argument('--permute_smiles', type=int, help='generates permutations of smiles', default=0)
+    parser.add_argument('--start', action='store_true')
     args = parser.parse_args()
     print(args)
     path = args.o
