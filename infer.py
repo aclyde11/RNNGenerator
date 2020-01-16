dac87d144f4b346e43d4087a6f19810454120a1c
Austin Clyde <aclyde@uchicago.edu>
2020-01-16 08:51:01 -0600

    converted to gpu code

--- infer.py
+++ infer.py
@@ -52,16 +52,16 @@ def sample(model, i2c, c2i, device, temp=1, batch_size=10, max_len=150):
     model.eval()
     with torch.no_grad():
 
-        c_0 = torch.zeros((4, batch_size, 256)).to(device)
-        # h_0 = torch.zeros((2, batch_size, 256)).to(device)
+        c_0 = torch.zeros((3, batch_size, 256)).to(device)
+        h_0 = torch.zeros((3, batch_size, 256)).to(device)
         x = torch.tensor(c2i(START_CHAR)).unsqueeze(0).unsqueeze(0).repeat((max_len, batch_size)).to(device)
 
         eos_mask = torch.zeros(batch_size, dtype=torch.bool).to(device)
         end_pads = torch.tensor([max_len - 1]).repeat(batch_size).to(device)
         for i in range(1, max_len):
             x_emb = model.emb(x[i - 1, :]).unsqueeze(0)
-            # o, (h_0, c_0) = model.lstm(x_emb, (h_0, c_0))
-            o, h_0 = model.lstm(x_emb, h_0)
+            o, (h_0, c_0) = model.lstm(x_emb, (h_0, c_0))
+            # o, h_0 = model.lstm(x_emb, h_0)
             y = model.linear(o.squeeze(0))
             y = F.softmax(y / temp, dim=-1)
             w = torch.multinomial(y, 1).squeeze()
